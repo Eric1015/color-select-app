47d51663f4b0738f5b71d5327e93b8a1f6a09fb8
love1 <love1dbsb77@hotmail.com>
2019-02-28 14:22:26 -0800

    Implemented the rule screen

--- screens/RuleScreen.js
+++ screens/RuleScreen.js
@@ -5,9 +5,16 @@ import {Container, Button, Text} from 'native-base';
 class RuleScreen extends React.Component {
     render() {
         return (
-            <Container style={styles.text_center}>
-                <Button bordered onPress={() => this.props.navigation.navigate('Home')} >
-                    <Text>
+            <Container style={styles.container}>
+                <Text>
+                    The rule is really easy. You will choose the correct color from the color button at the bottom.
+                    The correct color will depend on either what color the word meaning represents or what color the word is colored.
+                    In "normal" mode, this condition will change one by one. In "random" mode, the condition will change randomly,
+                    so you need to focus on what condition it is which is displayed in the top right of the game screen.
+                </Text>
+                <Text>{"\n"}</Text>
+                <Button success rounded onPress={() => this.props.navigation.navigate('Home')} style={styles.marginCenter}>
+                    <Text style={styles.boldText}>
                         Back to Home
                     </Text>
                 </Button>
@@ -17,11 +24,19 @@ class RuleScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    text_center: {
+    container: {
         alignItems: 'center',
         justifyContent: 'center',
         marginLeft: "auto",
+        marginRight: "auto",
+        width: "80%"
+    },
+    marginCenter: {
+        marginLeft: "auto",
         marginRight: "auto"
+    },
+    boldText: {
+        fontWeight: "bold"
     }
 });
 
